510be42c66375a1ab72a8e0a0485e1bb1c7f9d3b
Mohamed Mehdawy <mohammedmehdawy@gmail.com>
2023-05-24 08:49:04 +0300

    create new app [post]

---
+++ post/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+
+# Create your models here.
+
+class Post(models.Model):
+
+    title = models.CharField(max_length=200)
+    content = models.TextField(max_length=1500)
+    image = models.ImageField(default='post-img/')
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name = ("Post")
+        verbose_name_plural = ("Posts")
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse("Post_detail", kwargs={"pk": self.pk})
