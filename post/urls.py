5bccb857ea40aeac21d015ac70e7308600103495
Mohamed Mehdawy <mohammedmehdawy@gmail.com>
2023-05-25 00:59:08 +0300

    build post url structure

---
+++ post/urls.py
@@ -0,0 +1,11 @@
+
+from django.urls import path
+
+from . import views
+
+app_name = 'post'
+
+urlpatterns = [
+    path('', views.post_list, name='post_list'),
+    path('post_detail/<int:pk>/', views.post_detail, name='post_detail')
+]
--- post/views.py
+++ post/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
-
+from .models import Post
 # Create your views here.
+
+
+def post_list(request):
+    posts = Post.objects.all()
+    
+    print(posts)
+    
+    return
+    
+def post_detail(request, pk):
+    post = Post.objects.get(id=pk)
+    
+    print(post)
+    
+    return
